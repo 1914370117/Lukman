8c659b58f799c51a4b92032b07f3d65ff57eec91
Lukman Hakim <55613611+1914370117@users.noreply.github.com>
2019-10-31 00:57:36 -0700

    Add files via upload

---
+++ Lukman.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tBIODATA PRIBADI SAYA\n\n");
+    printf("NAMA : LUKMAN HAKIM\n");
+    printf("\NPM : 1914370117\n");
+    printf("\KELAS : REGULER 1D\n");
+    printf("\FAKULTAS : SAINS & TEKNOLOGI\n");
+    printf("\PRODI : SISKOM\n");
+    printf("\TUGAS 1\n");
+
+
+    int o, p, q;
+    o = 11;
+    p = 20;
+    q = o + p;
+    printf("Hasil dari %d + %d = %d.", o, p, q);
+{
+    int o, p, q;
+    o = 11;
+    p = 20;
+    q = o - p;
+    printf("Hasil dari %d - %d = %d.", o, p, q);
+}
+{
+    int o, p, q;
+    o = 11;
+    p = 20;
+    q = o * p;
+    printf("Hasil dari %d * %d = %d.", o, p, q);
+}
+{
+    int o, p, q;
+    o = 11;
+    p = 20;
+    q = o / p;
+    printf("Hasil dari %d / %d = %d.", o, p, q);
+}
+    return 0;
+}
